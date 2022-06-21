520d1a91e2a43ae537357354c99ed6abd829b695
EttoreBarcella <100206174+EttoreBarcella@users.noreply.github.com>
2022-06-21 15:25:36 +0200

    added text

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-
+    $text = "Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Laudantium fugiat in Lorem ad quasi cum numquam commodi Lorem repellendus velit delectus, 
+            omnis dolorum nam Lorem tempore ducimus esse odio Lorem.";
+    $text_censored = str_replace("Lorem", "***"; $text)
 ?>
 
 <!DOCTYPE html>
@@ -12,5 +15,11 @@
 </head>
 <body>
     
+    <h1>Uncensored text</h1>
+    <p><?= $text ?></p>
+
+    <h1>Censored text</h1>
+    <h3>Characters number: <?= strlen($text_censored) ?></h3>
+    <p><?= $text_censored ?></p>
 </body>
 </html>
